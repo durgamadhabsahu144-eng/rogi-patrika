27e72915cb17f7d912a6293aaf4a60f67c0db74c
Durga madhab Sahu <tunisahu43@gmail.com>
2026-09-01 19:15:25 +0000

    Before: if i am adding a patient why dashboard is not updating and Keep every addition aligned with SIH PS ...

---
+++ isolate/assets/search-Bk4egg71.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-CsajtpJ9.js";const c=[["path",{d:"M4 5h16",key:"1tepv9"}],["path",{d:"M4 12h16",key:"1lakjw"}],["path",{d:"M4 19h16",key:"1djgab"}]],t=e("menu",c);const a=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],h=e("search",a);export{t as M,h as S};
